bbc62ebe74d6b8d36c1aee12a8beffaeecd0399b
Martin Billinger <flkazemakase@gmail.com>
2018-03-26 20:29:04 +0200

    Commented example

--- examples/extra_trees_regressor.rs
+++ examples/extra_trees_regressor.rs
@@ -8,40 +8,48 @@ use gnuplot::*;
 
 use rand::{thread_rng, Rng};
 
-use forest::traits::*;
-use forest::api::extra_trees_regressor as etr;
+use forest::traits::Predictor;
+use forest::api::extra_trees_regressor::{ExtraTreesRegressor, Sample};
 
+/// function used to generate training data
 fn func(x: &f64) -> f64 {
     (x * 2.0 * PI).sin() + thread_rng().gen::<f64>() * 0.2
 }
 
+/// generate a Vec<f64> of linearly spaced values
 fn linspace(l: f64, h: f64, n: usize) -> Vec<f64> {
     let di = (h - l) / (n - 1) as f64;
     (0..n).map(|i| l + di * i as f64).collect()
 }
 
+/// generate a Vec<f64> of uniformly distributed random values
 fn randspace(l: f64, h: f64, n: usize) -> Vec<f64> {
     let mut rng = thread_rng();
     (0..n).map(|_| rng.gen_range(l, h)).collect()
 }
 
 fn main() {
-
+    // generate data points
     let x0 = randspace(0.0, 1.0, 100);
     let y0: Vec<_> = x0.iter().map(func).collect();
 
-    let mut data: Vec<etr::Sample<_, _>> = x0.iter().zip(y0.iter()).map(|(&x, &y)| etr::Sample::new([x], y)).collect();
-    let forest = etr::Builder::new(
-        100,
-        etr::TreeBuilder::new(
-            etr::SplitFitter::new(1, thread_rng()),
-            10,
-        )
-    ).fit(&mut data);
+    // convert data to data set for fitting
+    let mut data: Vec<_> = x0.iter().zip(y0.iter()).map(|(&x, &y)| Sample::new([x], y)).collect();
 
+    // configure and fit random forest
+    let forest = ExtraTreesRegressor::new()
+        .n_estimators(100)
+        .n_splits(1)
+        .min_samples_split(10)
+        .fit(&mut data);
+
+    // generate test data
     let x = linspace(-0.2, 1.2, 1000);
+
+    // predict y values
     let y: Vec<_> = x.iter().map(|&x| forest.predict(&[x])).collect();
 
+    // plot results
     let mut fig = Figure::new();
     fig.axes2d()
         .set_title("Extra Trees Regressor", &[])
