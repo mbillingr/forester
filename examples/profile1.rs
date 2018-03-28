41e3be39d2091253acb728c7629ab995fca18642
Martin Billinger <flkazemakase@gmail.com>
2018-03-29 00:09:58 +0200

    Made profiling optional

--- examples/profile1.rs
+++ examples/profile1.rs
@@ -1,10 +1,6 @@
-extern crate cpuprofiler;
 extern crate forester;
 extern crate rand;
-
 use std::f64::consts::PI;
-
-use cpuprofiler::PROFILER;
 
 use rand::{thread_rng, Rng};
 
@@ -25,28 +21,32 @@ fn randspace(l: f64, h: f64, n: usize) -> Vec<f64> {
     (0..n).map(|_| rng.gen_range(l, h)).collect()
 }
 
-fn main() {
-    let x0 = randspace(0.0, 1.0, 1000);
-    let y0: Vec<_> = x0.iter().map(func).collect();
+pub fn main() {
+    #[cfg(feature = "cpuprofiler")] {
+        extern crate cpuprofiler;
+        
+        let x0 = randspace(0.0, 1.0, 1000);
+        let y0: Vec<_> = x0.iter().map(func).collect();
 
-    let mut data: Vec<_> = x0.iter().zip(y0.iter()).map(|(&x, &y)| Sample::new([x], y)).collect();
+        let mut data: Vec<_> = x0.iter().zip(y0.iter()).map(|(&x, &y)| Sample::new([x], y)).collect();
 
-    let fb = ExtraTreesRegressor::new()
-        .n_estimators(100)
-        .n_splits(100)
-        .min_samples_split(2);
+        let fb = ExtraTreesRegressor::new()
+            .n_estimators(100)
+            .n_splits(100)
+            .min_samples_split(2);
 
-    PROFILER.lock().unwrap().start("fit.profile").unwrap();
-    let forest = fb.fit(&mut data);
-    PROFILER.lock().unwrap().stop().unwrap();
+        cpuprofiler::PROFILER.lock().unwrap().start("fit.profile").unwrap();
+        let forest = fb.fit(&mut data);
+        cpuprofiler::PROFILER.lock().unwrap().stop().unwrap();
 
-    let x = linspace(-0.2, 1.2, 10000);
+        let x = linspace(-0.2, 1.2, 10000);
 
-    PROFILER.lock().unwrap().start("predict.profile").unwrap();
-    let y: Vec<_> = x.iter().map(|&x| forest.predict(&[x])).collect();
-    PROFILER.lock().unwrap().stop().unwrap();
+        cpuprofiler::PROFILER.lock().unwrap().start("predict.profile").unwrap();
+        let y: Vec<_> = x.iter().map(|&x| forest.predict(&[x])).collect();
+        cpuprofiler::PROFILER.lock().unwrap().stop().unwrap();
 
-    println!("Profiling done. Convert the profile with something like");
-    println!("  > pprof --callgrind target/debug/examples/profile1 fit.profile > fit.prof");
-    println!("Or view it with\n  > pprof --gv target/debug/examples/profile1 fit.profile");
+        println!("Profiling done. Convert the profile with something like");
+        println!("  > pprof --callgrind target/debug/examples/profile1 fit.profile > fit.prof");
+        println!("Or view it with\n  > pprof --gv target/debug/examples/profile1 fit.profile");
+    }
 }
