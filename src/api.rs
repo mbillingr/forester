47fe2b3c8d0c48199ee6d39788a779f5ebe69e75
Martin Billinger <flkazemakase@gmail.com>
2018-03-30 12:36:12 +0200

    impl LearnerMut for ExtraTreesRegressor

--- src/api.rs
+++ src/api.rs
@@ -11,11 +11,11 @@ use features::ColumnSelect;
 use get_item::GetItem;
 use predictors::{CategoricalProbabilities, ClassPredictor, ConstMean};
 use splitters::{BestRandomSplit, ThresholdSplitter};
+use traits::LearnerMut;
 
 
 pub mod extra_trees_regressor {
     use super::*;
-    use traits::LearnerMut;
 
     pub type Builder<X, Y> = EnsembleBuilder<Y, Data<X, Y>, TreeBuilder<X, Y>, Tree<X, Y>>;
 
@@ -41,8 +41,8 @@ pub mod extra_trees_regressor {
     }
 
     impl ExtraTreesRegressor {
-        pub fn new() -> ExtraTreesRegressor {
-            ExtraTreesRegressor {
+        pub fn new() -> Self {
+            Self {
                 n_estimators: 10,
                 n_splits: 1,
                 min_samples_split: 1,
@@ -63,8 +63,20 @@ pub mod extra_trees_regressor {
             self.min_samples_split = n;
             self
         }
+    }
 
-        pub fn fit<X>(self, data: &mut Data<X, f64>) -> Model<X, f64>
+    impl Default for ExtraTreesRegressor {
+        fn default() -> Self {
+            Self::new()
+        }
+    }
+
+    impl<X> LearnerMut<Data<X, f64>, Model<X, f64>> for ExtraTreesRegressor
+        where X: Clone + GetItem,
+              X::Item: Clone + cmp::PartialOrd + SampleRange,
+    {
+
+        fn fit(&self, data: &mut Data<X, f64>) -> Model<X, f64>
             where X: Clone + GetItem,
                   X::Item: Clone + cmp::PartialOrd + SampleRange,
         {
@@ -102,6 +114,52 @@ pub mod extra_trees_classifier {
     pub type Predictor<X> =  ClassPredictor<Sample<X>>;
     pub type Features = ColumnSelect;
     pub type SplitCriterion<X> = GiniCriterion<Sample<X>>;
+
+    pub struct ExtraTreesClassifier {
+        n_estimators: usize,
+        n_splits: usize,
+        min_samples_split: usize,
+    }
+
+    impl ExtraTreesClassifier {
+        pub fn new() -> Self {
+            Self {
+                n_estimators: 10,
+                n_splits: 1,
+                min_samples_split: 1,
+            }
+        }
+
+        pub fn n_estimators(mut self, n: usize) -> Self {
+            self.n_estimators = n;
+            self
+        }
+
+        pub fn n_splits(mut self, n: usize) -> Self {
+            self.n_splits = n;
+            self
+        }
+
+        pub fn min_samples_split(mut self, n: usize) -> Self {
+            self.min_samples_split = n;
+            self
+        }
+
+        pub fn fit<X>(self, data: &mut Data<X>) -> Model<X>
+            where X: Clone + GetItem,
+                  X::Item: Clone + cmp::PartialOrd + SampleRange,
+        {
+            Builder::new(
+                self.n_estimators,
+                TreeBuilder::new(
+                    SplitFitter::new(
+                        self.n_splits,
+                        thread_rng()),
+                    self.min_samples_split,
+                )
+            ).fit(data)
+        }
+    }
 }
 
 
@@ -113,13 +171,18 @@ mod tests {
         use super::extra_trees_regressor::ExtraTreesRegressor;
         use super::extra_trees_regressor::Sample;
         use Predictor;
+        use traits::LearnerMut;
 
         let x = vec![[1], [2], [3],    [7], [8], [9]];
         let y = vec![5.0, 5.0, 5.0,    2.0, 2.0, 2.0];
 
         let mut data: Vec<_> = x.into_iter().zip(y.into_iter()).map(|(x, y)| Sample::new(x, y)).collect();
 
-        let model = ExtraTreesRegressor::new().fit(&mut data);
+        let model = ExtraTreesRegressor::new()
+            .n_estimators(2)
+            .n_splits(1)
+            .min_samples_split(2)
+            .fit(&mut data);
 
         assert_eq!(model.predict(&[-1000]), 5.0);
         assert_eq!(model.predict(&[1000]), 2.0);
