1d25bcff609fb90295c9be582cafa4ccb4668bdc
Martin Billinger <flkazemakase@gmail.com>
2018-04-01 20:48:11 +0200

    faster unsafe impl of Partition

--- benches/partition.rs
+++ benches/partition.rs
@@ -3,24 +3,41 @@ extern crate criterion;
 extern crate rand;
 extern crate forester;
 
-use std::f64;
-
 use criterion::Criterion;
 use criterion::Bencher;
 
 use forester::array_ops::Partition;
 
+fn safe_partition<T, F: FnMut(&T) -> bool>(x: &mut [T], mut predicate: F) -> usize {
+    let mut target_index = 0;
+    for i in 0..x.len() {
+        if predicate(&x[i]) {
+            x.swap(target_index, i);
+            target_index += 1;
+        }
+    }
+    target_index
+}
+
 
 fn bench_partition(c: &mut Criterion) {
-    fn function(b: &mut Bencher) {
-        let mut x: Vec<_> = (1..1000).collect();
+    fn function1(b: &mut Bencher) {
+        let x: Vec<_> = (1..1000).collect();
         b.iter(|| {
             x.clone().partition(|&i| i % 2 == 0);
         })
     }
+    c.bench_function("Partition (unsafe)", function1);
 
-    c.bench_function("Partition", function);
+
+    fn function2(b: &mut Bencher) {
+        let x: Vec<_> = (1..1000).collect();
+        b.iter(|| {
+            safe_partition(&mut x.clone(), |&i| i % 2 == 0);
+        })
+    }
+    c.bench_function("Partition (safe)", function2);
 }
 
-criterion_group!(benches, compare_fibonaccis);
+criterion_group!(benches, bench_partition);
 criterion_main!(benches);
