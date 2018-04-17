171b79f58292a4488ce9e9a8d456814e918c9639
_ _ <_@_._>
2018-04-17 16:51:03 +0100

    Updated Extra Trees Regressor example to new API

--- examples/extra_trees_regressor.rs
+++ examples/extra_trees_regressor.rs
@@ -10,6 +10,7 @@ use rand::{thread_rng, Rng};
 
 use forester::traits::{LearnerMut, Predictor};
 use forester::api::extra_trees_regressor::{ExtraTreesRegressor, Sample};
+use forester::vec2d::Vec2D;
 
 /// function used to generate training data
 fn func(x: &f64) -> f64 {
@@ -33,15 +34,15 @@ fn main() {
     let x0 = randspace(0.0, 1.0, 100);
     let y0: Vec<_> = x0.iter().map(func).collect();
 
-    // convert data to data set for fitting
-    let mut data: Vec<_> = x0.iter().zip(y0.iter()).map(|(&x, &y)| Sample::new([x], y)).collect();
+    let x_train = Vec2D::from_slice(&x0, 1);
+    let y_train = y0.clone();
 
     // configure and fit random forest
     let forest = ExtraTreesRegressor::new()
         .n_estimators(100)
         .n_splits(1)
         .min_samples_split(10)
-        .fit(&mut data);
+        .fit(&x_train, &y_train);
 
     // generate test data
     let x = linspace(-0.2, 1.2, 1000);
