acfb3dddebf1c21a5b9544fd12321fd7b0d48763
Martin Billinger <flkazemakase@gmail.com>
2018-04-22 17:49:33 +0200

    Removed all the old traits and types

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,8 @@ extern crate rand;
 
 pub mod api;
 pub mod array_ops;
-pub mod criteria;
-pub mod datasets;
-pub mod features;
-pub mod ensemble;
-pub mod get_item;
 pub mod iter_mean;
-pub mod predictors;
-pub mod random;
-pub mod splitters;
-pub mod traits;
-pub mod d_tree;
 pub mod vec2d;
-
-type Real = f64;
-
-pub use traits::*;
 
 
 use std::marker::PhantomData;
